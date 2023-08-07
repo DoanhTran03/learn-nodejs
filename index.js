6152081975fb2d2a4640a1afe7f9a1dc0da3e6a1
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-07 12:49:43 -0400

    Handling GET request

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res) => {
-    res.send('Hello, welcome to our app!')
-})
+const books = [{ name: "Harry Potter", price: 29 }, { name : "Story Book", price: 18}, { name: "Monospace", price: 40}];
 
-app.get('/books', (req,res) => {
-    res.send(['Harry Potter, Space Mono, The Story'])
-})
+app.get("/", (req, res) => {
+  res.send("Hello, welcome to our app!");
+});
 
-app.get('/books/:month/:year', (req,res) => {
-    res.send(req.params);
-})
+app.get("/books", (req, res) => {
+  res.send(["Harry Potter, Space Mono, The Story"]);
+});
+
+app.get("/books/:price", (req, res) => {
+  const foundBook = books.find(book => book.price === parseInt(req.params.price));
+  if(!foundBook) res.status(400).send("The book you is not available yet!");
+  res.send(foundBook);
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: http://localhost:${PORT}`)
-})
+  console.log(`Listening on port: http://localhost:${PORT}`);
+});
