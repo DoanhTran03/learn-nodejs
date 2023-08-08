b149adaaebc239a054a98c91e89e9182ca2960a6
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-08 00:44:35 -0400

    Handling POST request with joi 13th schema validator

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const Joi = require("joi");
 const express = require("express");
+const { schema } = require("joi/lib/types/object");
 const app = express();
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-const books = [{ name: "Harry Potter", price: 29 }, { name : "Story Book", price: 18}, { name: "Monospace", price: 40}];
+const books = [
+  { id: 1, name: "Harry Potter", price: 29 },
+  { id: 2, name: "Story Book", price: 18 },
+  { id: 3, name: "Monospace", price: 40 },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello, welcome to our app!");
@@ -14,9 +22,28 @@ app.get("/books", (req, res) => {
 });
 
 app.get("/books/:price", (req, res) => {
-  const foundBook = books.find(book => book.price === parseInt(req.params.price));
-  if(!foundBook) res.status(400).send("The book you is not available yet!");
+  const foundBook = books.find(
+    (book) => book.price === parseInt(req.params.price)
+  );
+  if (!foundBook) res.status(400).send("The book you is not available yet!");
   res.send(foundBook);
+});
+
+app.post("/books", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  result = Joi.validate(req.body,schema);
+  if (result.error) {
+    res.send(result.error.details[0].message)
+    return;
+  }
+  const book = {
+    id: books.length + 1,
+    name: req.body.name,
+  };
+  books.push(book);
+  res.send(book);
 });
 
 app.listen(PORT, () => {
