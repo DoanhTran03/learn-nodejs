a7ea93009ca8c94a90f86e460cd38292bea1deac
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-10 05:52:02 -0400

    Using callback to get user repositories

--- callback.js
+++ callback.js
@@ -1,13 +1,23 @@
 //Using call back function
-console.log('Before')
-getUserData(10, (user) => {
-    console.log(user);
-});
-console.log('After')
-function getUserData (id, callback) {
-    setTimeout(()=>{
-        console.log(`User data fetched`);
-        callback({id: id, name: 'David'});
-    },2000);
-} 
+// console.log('Before')
+// getUserData(10, (user) => {
+//     console.log(user);
+// });
+// console.log('After')
+// function getUserData (id, callback) {
+//     setTimeout(()=>{
+//         console.log(`User data fetched`);
+//         callback({id: id, name: 'David'});
+//     },2000);
+// } 
 
+getRepositories('Robert', (repositories) => {
+    console.log(repositories);  
+})
+
+function getRepositories (username, callback) {
+    setTimeout(()=>{
+        console.log('Getting user repositories');
+        callback(['repo1','repo2','repo3'])
+    },2000)
+}
