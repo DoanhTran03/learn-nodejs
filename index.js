585e429b0cb0860250b541cf952342ab0fb38f12
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-11 23:15:30 -0400

    Create course schema in index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
 .then(() => console.log('Connected to mongoDB'))
-.catch((err) => console.log('Can not connect to mongoD ', err))
+.catch((err) => console.log('Can not connect to mongoD ', err)) 
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+})
+console.log(courseSchema)
