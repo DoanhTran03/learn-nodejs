518104bae15e3b6199b83c3873bed0e6ca346e74
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-21 00:52:11 -0400

    Refactoring customers model in models folder

--- project/routes/customers.js
+++ project/routes/customers.js
@@ -1,20 +1,6 @@
-const mongoose = require("mongoose");
 const express = require("express");
-const Joi = require("joi");
 const router = express.Router();
-
-const CustomerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 3,
-    maxlength: 10,
-    required: true,
-  },
-  phone: { type: String, minlength: 3, maxlength: 15, required: true },
-  isGold: { type: Boolean, required: true },
-});
-
-const Customer = mongoose.model("customer", CustomerSchema);
+const {Customer, validateCustomer} = require('../models/customer')
 
 router.get("/", async (req, res) => {
   const customers = await Customer.find({}).sort({ name: 1 }).lean();
@@ -22,7 +8,6 @@ router.get("/", async (req, res) => {
 });
 
 router.post('/', async (req,res) => {
-    console.log('Hello');
     const {error} = validateCustomer(req.body);
     if (error) return res.status(404).send(error.details[0].message);
     const customer = new Customer(req.body);
@@ -51,12 +36,4 @@ router.delete('/:id', async (req, res) => {
     res.send(result);
 })
 
-const validateCustomer = (customer) => {
-  const schema = {
-    name: Joi.string().min(3).max(10).required(),
-    phone: Joi.string().min(3).max(15).required(),
-    isGold: Joi.boolean().required(),
-  };
-  return Joi.validate(customer,schema);
-};
 module.exports = router;
