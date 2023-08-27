be01ade637f3f3eed851b011a9ac0a0550b16788
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-27 19:29:15 -0400

    Authenticate user in auth.js in vividly project

--- vividly/index.js
+++ vividly/index.js
@@ -5,12 +5,14 @@ Joi.objectId = require('joi-objectid')(Joi);
 const genre = require('./routes/genre');
 const movie = require('./routes/movie');
 const user = require('./routes/user');
+const auth = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
 app.use('/api/genres', genre);
 app.use('/api/movies', movie);
 app.use('/api/users', user);  
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 3000;
 
--- vividly/routes/auth.js
+++ vividly/routes/auth.js
@@ -1,1 +1,19 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const {User, validateUser} = require('../model/user');
+const bcrypt = require('bcrypt');
+
+router.post('/', async (req,res) => {
+    const {error} = validateUser(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.send('The user with given email was not found');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password );
+    if (!validPassword) return res.status(404).send('Password is not correct');
+    
+    return res.send(true);
+})
+
+module.exports   = router;
