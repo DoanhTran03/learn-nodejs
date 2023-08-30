448560a294f0a3b83b786ad4768e4f5f6d7e1aa1
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-30 18:35:54 -0400

    Uncaught error and unhandled error exception handler in index.js

--- vividly/index.js
+++ vividly/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const winston = require('winston');
+require('winston-mongodb');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const config = require('config');
@@ -18,7 +19,18 @@ app.use('/api/users', user);
 app.use('/api/auth', auth);
 app.use(error);
 
+process.on('uncaughtException', (ex) => {
+  winston.error(ex.message,ex);
+  process.exit(1);
+})
+
+process.on('unhandledRejection', (ex) => {
+  throw ex;
+})
+
 winston.add(winston.transports.File, {filename: 'logger.txt'});
+
+const p = Promise.reject(new Error('rejected error'));
 
 const PORT = process.env.PORT || 3000;
 
