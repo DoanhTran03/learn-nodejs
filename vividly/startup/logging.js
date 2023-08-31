09b277fe0e2f6b634da040addf29ae0979a69ba9
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-31 07:27:42 -0400

    Add Console as winston transport in startup-logging

--- vividly/startup/logging.js
+++ vividly/startup/logging.js
@@ -1,7 +1,13 @@
 const config = require("config");
 const winston = require('winston');
+require('winston-mongodb');
 module.exports = function () {
-  winston.add(winston.transports.File, {filename: 'logger.txt'});
+  winston.configure({
+    transports: [
+        new (winston.transports.Console)({colorize:true, prettyPrint: true}),
+        new (winston.transports.File)({ filename: 'logs/warn.log', name: 'file.error', level: 'error'}),
+    ]
+    });
 
   process.on("uncaughtException", (ex) => {
     winston.error(ex.message, ex);
@@ -12,6 +18,6 @@ module.exports = function () {
     throw ex;
   });
   if (!config.get("jwtPrivateKey")) {
-    throw Error("Private Key is missing");
+    throw yError("Private Key is missing");
   }
 };
