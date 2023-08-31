0694b1e97ccba682572f73c10bbabe4fd5fc42d9
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-30 22:19:44 -0400

    Refactor routes and db startup in startup folder

--- vividly/index.js
+++ vividly/index.js
@@ -5,19 +5,10 @@ require('winston-mongodb');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const config = require('config');
-const genre = require('./routes/genre');
-const movie = require('./routes/movie');
-const user = require('./routes/user');
-const auth = require('./routes/auth');
-const error = require('./middlewares/error');
 
 const app = express();
-app.use(express.json());
-app.use('/api/genres', genre);
-app.use('/api/movies', movie);
-app.use('/api/users', user);  
-app.use('/api/auth', auth);
-app.use(error);
+require('./startup/routes')(app);
+require('./startup/db')();
 
 process.on('uncaughtException', (ex) => {
   winston.error(ex.message,ex);
@@ -38,10 +29,5 @@ if(!config.get('jwtPrivateKey')) {
     console.log('Private Key is missing');
     process.exit(1);
 }
-
-mongoose
-  .connect("mongodb://localhost/vividly")
-  .then(() => console.log("Connected to vividly database"))
-  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`Backend is listening on http://localhost/${PORT}`));
---
+++ vividly/startup/routes.js
@@ -0,0 +1,13 @@
+const genre = require("./routes/genre");
+const movie = require("./routes/movie");
+const user = require("./routes/user");
+const auth = require("./routes/auth");
+const error = require("./middlewares/error");
+module.exports = function (app) {
+  app.use(express.json());
+  app.use("/api/genres", genre);
+  app.use("/api/movies", movie);
+  app.use("/api/users", user);
+  app.use("/api/auth", auth);
+  app.use(error);
+};
