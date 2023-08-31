8410eddd85488611f5f371d83d43f73fdda2c74b
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-30 22:34:21 -0400

    Refactor validation startup in startup folder

--- vividly/index.js
+++ vividly/index.js
@@ -1,26 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const winston = require('winston');
-require('winston-mongodb');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const config = require('config');
-
 const app = express();
+
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/logging')();
-
-process.on('uncaughtException', (ex) => {
-  winston.error(ex.message,ex);
-  process.exit(1);
-})
-
-process.on('unhandledRejection', (ex) => {
-  throw ex;
-})
-
-winston.add(winston.transports.File, {filename: 'logger.txt'});
+require('./startup/validation')();  
 
 const PORT = process.env.PORT || 3000;
 
--- vividly/startup/logging.js
+++ vividly/startup/logging.js
@@ -1,6 +1,17 @@
-const config = require('config');
+const config = require("config");
+const winston = require('winston');
 module.exports = function () {
-    if(!config.get('jwtPrivateKey')) {
-        throw Error ('Private Key is missing');
-    }
-}
+  winston.add(winston.transports.File, {filename: 'logger.txt'});
+
+  process.on("uncaughtException", (ex) => {
+    winston.error(ex.message, ex);
+    process.exit(1);
+  });
+
+  process.on("unhandledRejection", (ex) => {
+    throw ex;
+  });
+  if (!config.get("jwtPrivateKey")) {
+    throw Error("Private Key is missing");
+  }
+};
